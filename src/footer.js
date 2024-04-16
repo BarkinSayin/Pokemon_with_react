1864ab03159528ca53161ab85ab06f25e7b988c3
BarkinSayin <barkinsayin@hotmail.com>
2024-04-16 15:14:09 +0300

    footer-component

---
+++ src/footer.js
@@ -0,0 +1,22 @@
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="footer-icons">
+        <a href="*">
+          <i className="bi bi-facebook"></i>
+        </a>
+        <a href="*">
+          <i className="bi bi-instagram"></i>
+        </a>
+        <a href="*">
+          <i className="bi bi-youtube"></i>
+        </a>
+      </div>
+      <div className="footer-text">&copy; Copyright 2022 By Pikachu</div>
+    </div>
+  );
+}
+
+export default Footer;

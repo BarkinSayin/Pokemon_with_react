7787800a6b7c40818b0852e29af2d3ca5b3463cf
BarkinSayin <barkinsayin@hotmail.com>
2024-04-16 14:50:49 +0300

    create_useStates

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
+import { useState, useEffect } from "react";
 import Header from "header.js";
 import Inputs from "inputs.js";
 
 function App() {
   const pokemonApiURL = "https://pokeapi.co/api/v2/";
+
+  const [pokemonInput, setPokemonInput] = useState({
+    pokemonName: "",
+    typeName: "",
+  });
+  const [pokemonList, setPokemonList] = useState([]);
 
   return (
     <div className="App">
